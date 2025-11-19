813bf653a436eb81a039b2bf7c5a5808df40a649
ppxb <317842449@qq.com>
2025-11-19 10:32:14 +0800

    chore: add global configuration

--- pkg/global/global.go
+++ pkg/global/global.go
@@ -1,5 +1,17 @@
 package global
 
+import (
+	"go.opentelemetry.io/otel/trace"
+	"gorm.io/gorm"
+
+	"github.com/ppxb/oreo-admin-go/pkg/config"
+)
+
 var (
+	Mode        string
 	RuntimeRoot string
+	Conf        Configuration
+	ConfBox     config.ConfBox
+	Tracer      *trace.TracerProvider
+	Mysql       *gorm.DB
 )

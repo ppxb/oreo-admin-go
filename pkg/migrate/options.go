818906b70d1516605b2b2fc3de3973ae4c047008
ppxb <317842449@qq.com>
2025-11-20 10:31:41 +0800

    chore: add migrate options

---
+++ pkg/migrate/options.go
@@ -0,0 +1,83 @@
+package migrate
+
+import (
+	"context"
+	"embed"
+
+	"github.com/ppxb/oreo-admin-go/pkg/utils"
+)
+
+type Options struct {
+	ctx         context.Context
+	driver      string
+	uri         string
+	lockName    string
+	before      func(ctx context.Context) error
+	changeTable string
+	fs          embed.FS
+	fsRoot      string
+}
+
+func WithCtx(ctx context.Context) func(*Options) {
+	return func(options *Options) {
+		if !utils.InterfaceIsNil(options.ctx) {
+			getOptionsOrSetDefault(options).ctx = options.ctx
+		}
+	}
+}
+
+func WithDriver(s string) func(*Options) {
+	return func(options *Options) {
+		getOptionsOrSetDefault(options).driver = s
+	}
+}
+
+func WithUri(s string) func(*Options) {
+	return func(options *Options) {
+		getOptionsOrSetDefault(options).uri = s
+	}
+}
+
+func WithLockName(s string) func(*Options) {
+	return func(options *Options) {
+		getOptionsOrSetDefault(options).lockName = s
+	}
+}
+
+func WithBefore(f func(ctx context.Context) error) func(*Options) {
+	return func(options *Options) {
+		if f != nil {
+			getOptionsOrSetDefault(options).before = f
+		}
+	}
+}
+
+func WithChangeTable(s string) func(*Options) {
+	return func(options *Options) {
+		getOptionsOrSetDefault(options).changeTable = s
+	}
+}
+
+func WithFs(fs embed.FS) func(*Options) {
+	return func(options *Options) {
+		getOptionsOrSetDefault(options).fs = fs
+	}
+}
+
+func WithFsRoot(s string) func(*Options) {
+	return func(options *Options) {
+		getOptionsOrSetDefault(options).fsRoot = s
+	}
+}
+
+func getOptionsOrSetDefault(options *Options) *Options {
+	if options == nil {
+		return &Options{
+			driver:      "mysql",
+			uri:         "root:123456@tcp(127.0.0.1:3306)/oreo_admin_go?charset=utf8mb4&collation=utf8mb4_general_ci&parseTime=True&loc=UTC&timeout=10000ms",
+			lockName:    "MigrationLock",
+			changeTable: "schema_migrations",
+		}
+	}
+	return options
+}
